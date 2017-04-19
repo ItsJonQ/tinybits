32431cedf4aebba3f471a7a05eb9458abc910a30
ItsJonQ <itsjonq@gmail.com>
2017-04-18 20:39:27 -0400

    Add alternate sizes to the stone tile

--- scripts/build-dev.js
+++ scripts/build-dev.js
@@ -9,6 +9,7 @@ var sass = require('node-sass');
 var file = pkg.name;
 var includePaths = pathfinder([
   require('seed-dash'),
+  require('seed-props'),
   require('seed-publish'),
   require('seed-this'),
 ]);
